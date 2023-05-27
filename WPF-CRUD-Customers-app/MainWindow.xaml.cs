a5c2db36d2461baefb3d558004c7f821b7419e60
Jakub Faltyn <86327309+JakubFaltyn@users.noreply.github.com>
2023-05-27 20:14:07 +0200

    MAin window added with working app

---
+++ WPF-CRUD-Customers-app/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_CRUD_Customers_app.Data;
+
+namespace WPF_CRUD_Customers_app
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CustomerDbContext context;
+        Customer NewCustomer = new Customer();
+        Customer selectedCustomer = new Customer();
+
+
+        public MainWindow(CustomerDbContext context)
+        {
+            this.context = context;
+            InitializeComponent();
+            GetCustomers();
+            NewCustomerGrid.DataContext = NewCustomer;
+        }
+
+
+        private void GetCustomers()
+        {
+            CustomerDG.ItemsSource = context.Customers.ToList();
+        }
+
+        private void AddItem(object s, RoutedEventArgs e)
+        {
+            context.Customers.Add(NewCustomer);
+            context.SaveChanges();
+            GetCustomers();
+            NewCustomer = new Customer();
+            NewCustomerGrid.DataContext = NewCustomer;
+        }
+
+        private void UpdateItem(object s, RoutedEventArgs e)
+        {
+            context.Update(selectedCustomer);
+            context.SaveChanges();
+            GetCustomers();
+        }
+
+        private void SelectCustomerToEdit(object s, RoutedEventArgs e)
+        {
+            selectedCustomer = (s as FrameworkElement).DataContext as Customer;
+            UpdateCustomerGrid.DataContext = selectedCustomer;
+        }
+
+        private void DeleteCustomer(object s, RoutedEventArgs e)
+        {
+            var customerToDelete = (s as FrameworkElement).DataContext as Customer;
+            context.Customers.Remove(customerToDelete);
+            context.SaveChanges();
+            GetCustomers();
+        }
+    }
+}
