de93245aa8cdd203215e67d0731c79f3365b90b2
Jakub Faltyn <86327309+JakubFaltyn@users.noreply.github.com>
2023-05-27 20:21:52 +0200

    Comments added to Customers

--- WPF-CRUD-Customers-app/Data/Customer.cs
+++ WPF-CRUD-Customers-app/Data/Customer.cs
@@ -6,12 +6,34 @@ using System.Threading.Tasks;
 
 namespace WPF_CRUD_Customers_app.Data
 {
+    /// <summary>
+    /// Represents a customer entity.
+    /// </summary>
     public class Customer
     {
+        /// <summary>
+        /// Gets or sets the customer ID.
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the customer's first name.
+        /// </summary>
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the customer's surname.
+        /// </summary>
         public string Surname { get; set; }
+
+        /// <summary>
+        /// Gets or sets the customer's age.
+        /// </summary>
         public double Age { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of books owned by the customer.
+        /// </summary>
         public int Books { get; set; }
     }
 }
